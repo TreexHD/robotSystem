896059d1859e49bb9857e1c0ab71a07050616eb3
TreexHD <67122079+TreexHD@users.noreply.github.com>
2024-09-29 00:37:55 +0200

    edit setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,8 @@ setup(
     author="TreexHD",
     author_email="<no.mail>",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    install_requires=[],
     keywords=['python'],
     classifiers=[
         "Development Status :: 1 - Planning",
