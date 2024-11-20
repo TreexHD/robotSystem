8d301660caee280c9ffb6f4f2083909134ea942c
TreexHD <67122079+TreexHD@users.noreply.github.com>
2024-11-20 16:18:29 +0100

    tof init updates

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 DESCRIPTION = 'this is the robot System library'
 LONG_DESCRIPTION = 'A package that allows easy access to all functions of the robot'
 
