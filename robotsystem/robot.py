a69f99f83231075259c8ee4749351436bd3b42e4
TreexHD <67122079+TreexHD@users.noreply.github.com>
2025-06-20 14:16:50 +0200

    sensor direction back

--- robotsystem/robot.py
+++ robotsystem/robot.py
@@ -18,13 +18,13 @@ import robotsystem.debug
 import robotsystem.bus
 
 # CONST
-LL = 5
-L = 4
+LL = 1
+L = 2
 M = 3
-R = 2
-RR = 1
-LG = 7
-RG = 6
+R = 4
+RR = 5
+LG = 6
+RG = 7
 
 
 def map_range(x, in_min, in_max, out_min, out_max):
